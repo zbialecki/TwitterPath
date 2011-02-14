555a75fc62e4b93307bcf9010acb7ed23d449d9a
Zach Bialecki <zach.bialecki@gmail.com>
2011-02-14 01:41:36 -0500

    Added a settings template to make setting this up locally slightly less confusing

---
+++ app/settings_template.js
@@ -0,0 +1,12 @@
+// This is a template. Fill in with values from your Twitter app.
+
+// Twitter App Settings
+var T = {}
+T.ACCESS_TOKEN = ''
+T.ACCESS_TOKEN_SECRET = ''
+
+T.CONSUMER_KEY = ''
+T.CONSUMER_SECRET = ''
+
+T.API_KEY = ''
+exports.T = T
